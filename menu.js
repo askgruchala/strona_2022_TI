56abbb1c9e58ab77266302c2a4c8328b3eb0f0b9
askgruchala <askgruchala@wp.pl>
2022-09-09 22:35:33 +0200

    z1

---
+++ menu.js
@@ -0,0 +1,36 @@
+let adres='http://127.0.0.1:5500/';
+            
+
+//let adres='http://gorka.hostng.pl/2pti/';
+
+document.write(
+//div główny - koniec w stopce
+'<div style="width:80%; margin:auto;">'+ 
+
+//div menu
+'<div style="text-align:center; background-color: #6f6b69; padding: 15px;">'+
+
+'<a href="'+adres+'index.html"><img src="'+adres+'img/home.png" alt="2pti" title="index"></a>'+
+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
+    
+'<a href="'+adres+'video.html"><img src="'+adres+'img/video.png" alt="2pti" title="video"></a>'+              '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
+    
+'<a href="'+adres+'hobby.html"><img src="'+adres+'img/hobby.png" alt="hobby" title="hobby"></a>'+
+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
+
+'<a href="'+adres+'galeria/thumb.html"><img src="'+adres+'img/galeria.png" alt="galeria" title="galeria"></a>'+
+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
+    
+'<a href="'+adres+'kontakt.html"><img src="'+adres+'img/kontakt.png" alt="kontakt" title="kontakt"></a>'+
+'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+
+
+    '<br><img src="'+adres+'linia.png" alt="2pti" style="width: 100%"></div>'+
+
+//div treść stron
+'<div style="background-color: #bfac86; padding: 15px; min-height: 350px;">'
+              
+              
+);    
+
+
+
---
+++ stopka.js
@@ -0,0 +1,15 @@
+document.write(
+//koniec div tresc
+'</div>'+
+
+//div stopka
+'<div style="text-align:right; background-color: #6f6b69; padding: 15px;">'+
+'<a href="'+adres+'kontakt.html"><img src="'+adres+'img/przycisk.png" alt="2pti"></a>'+
+'</div>'+ 
+
+//koniec div główny
+'</div>' 
+);    
+
+
+
